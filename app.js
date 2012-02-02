9c78aa758aa2b254a5685ae74bb7ed8920cc32fb
Evan Hazlett <ejhazlett@gmail.com>
2012-02-02 17:02:53 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+var APP_PORT = 3000;
+
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+io.configure('development', function(){
+  io.set('transports', ['websocket']);
+});
+
+
+app.listen(APP_PORT);
+
+function handler (req, res) {
+  var urlParts = url.parse(req.url, true);
+  route(req, urlParts, res);
+}
+
+function route(req, parts, res){
+  var filename = null;
+  console.log(parts.path);
+  switch(parts.path) {
+    case '/':
+      filename = 'templates/index.html';
+      break;
+    case '/static/style.css':
+      filename = 'static/style.css';
+      break;
+    case '/static/bootstrap-responsive.min.css':
+      filename = 'static/bootstrap-responsive.min.css';
+      break;
+    case '/receive':
+      if (req.method == 'POST') {
+        var postData = ''; 
+        req.addListener('data', function(chunk){
+          postData += chunk.toString();
+        }).addListener('end', function() {
+          var post = qs.parse(postData);
+          io.sockets.emit('events', post);
+        });
+      }   
+      res.writeHead(200);
+      return res.end('received');
+      break;
+    default:
+      res.writeHead(404);
+      res.write('Unknown');
+      break;
+  }
+  fs.readFile(__dirname + '/' + filename,
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading ' + filename);
+    }   
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+try {
+  io.sockets.on('connection', function (socket) {
+    socket.on('client', function(data){
+      console.log(data);
+    });
+  });
+} catch(err) {
+  console.log(err);
+}
+console.log('Informant running on port ' + APP_PORT + '...');
