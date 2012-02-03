5d20fd9838c5ade052fa29e4021a8bf27c276048
Evan Hazlett <ejhazlett@gmail.com>
2012-02-03 16:17:31 -0500

    started on oauth

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var qs = require('querystring');
 var APP_PORT = 3000;
@@ -7,10 +8,52 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
 
-io.configure('development', function(){
-  io.set('transports', ['websocket']);
+var CONFIG;
+var INTERVALS = {};
+
+// load config
+fs.readFile(__dirname + '/config.json',
+  function (err, data) {
+    if (err) {
+      console.log("Unable to load config.json: " + err);
+    } else {
+      CONFIG = JSON.parse(data.toString());
+      if (CONFIG.hasOwnProperty('repos')){
+        setupGithubChecks();
+      }
+    }
 });
 
+function setupGithubChecks(){
+  for (var i in CONFIG.repos) {
+    var repo = CONFIG.repos[i];
+    console.log('Setting up Github checks for ' + repo.name);
+    var interval = setInterval(function(){
+      var headers = null;
+      if (repo.hasOwnProperty('authUser')){
+        console.log('Using auth for ' + repo.name);
+        headers = {
+          'Authorization': 'Basic ' + new Buffer(repo.authUser + ':' + repo.authPass).toString('base64')
+        }
+      }
+      var req = https.request({
+        host: 'api.github.com',
+        port: 443,
+        path: '/repos/'+repo.owner+'/'+repo.name+'/pulls',
+        //path: '/users/'+repo.owner+'/repos',
+        method: 'GET',
+        headers: headers
+      }, function(res){
+        res.on('data', function(data){
+          console.log(data.toString());
+        });
+      }); 
+      req.end();
+      //console.log(req);
+    }, 5000);
+    INTERVALS[repo.name] = interval;
+  }
+}
 
 app.listen(APP_PORT);
 
@@ -55,6 +98,26 @@ function route(req, parts, res){
         }).addListener('end', function() {
           var post = qs.parse(postData);
           io.sockets.emit('events', post);
+        });
+      }   
+      res.writeHead(200);
+      return res.end('received');
+      break;
+    case '/addrepo':
+      var url = 'https://github.com/login/oauth/authorize?client_id='+CONFIG.githubClientId;
+      console.log(url);
+      res.writeHead(302, {
+        'Location': url
+      });
+      res.end();
+      break;
+    case '/authorize':
+      if (req.method == 'POST') {
+        var postData = ''; 
+        req.addListener('data', function(chunk){
+          postData += chunk.toString();
+        }).addListener('end', function() {
+          console.log(postData);
         });
       }   
       res.writeHead(200);
